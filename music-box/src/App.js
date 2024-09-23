f37b3f8026d114e23d6dd103f1f940721b9d0176
rafascarabe <rafaela.scarabe@sptech.school>
2024-09-23 18:04:22 -0300

    aula 03

--- music-box/src/App.js
+++ music-box/src/App.js
@@ -1,31 +1,57 @@
 
+// import api from "./api";
+// function App() {
+
+//   function listar(){
+//     api.get() //requisição assincrona
+//         .then((respostaObtida) => { 
+//           // cairá aqui se a requisição for realizada;  
+//         console.log(respostaObtida); 
+//           // objeto que representa a resposta enviada pela API;  
+//         console.log(respostaObtida.status); 
+//           // vendo status da resposta (OK - 200);  
+//         console.log(respostaObtida.data); 
+//           // vendo os dados da resposta (data: []); 
+//         //setMusicas(respostaObtida.data) 
+//           // setando "musicas" com os mesmos dados recebidos pela resposta da requisição; 
+//         }) 
+//         .catch((erroOcorrido) => { // cairá aqui se houver algum erro durante a requisição 
+//         console.log(erroOcorrido); 
+// })
+//   }
+//   return (
+
+//     <>
+//     <h1>Titulo</h1>
+//     <button onClick={listar}>Listar</button>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
 import api from "./api";
 function App() {
-  
-  function listar(){
-    api.get() 
-        .then((respostaObtida) => { 
-          // cairá aqui se a requisição for realizada;  
-        console.log(respostaObtida); 
-          // objeto que representa a resposta enviada pela API;  
-        console.log(respostaObtida.status); 
-          // vendo status da resposta (OK - 200);  
-        console.log(respostaObtida.data); 
-          // vendo os dados da resposta (data: []); 
-        //setMusicas(respostaObtida.data) 
-          // setando "musicas" com os mesmos dados recebidos pela resposta da requisição; 
-        }) 
-        .catch((erroOcorrido) => { // cairá aqui se houver algum erro durante a requisição 
-        console.log(erroOcorrido); 
-})
-  }
-  return (
 
+  
+  function listar() {
+    api
+    .get()
+      .then((respostaObtida) => {
+
+        console.log(respostaObtida);
+      })
+      .catch((erroOcorrido) => {
+        // cairá aqui se houver algum erro durante a requisição 
+        console.log(erroOcorrido);
+      })
+  }
+  return(
     <>
-    <h1>Titulo</h1>
-    <button onClick={listar}>Listar</button>
+      <h1>Titulo</h1>
+      <button onClick={listar}>Listar</button>
     </>
   );
 }
-
-export default App;
+export default App;  
--- music-box/src/api.js
+++ music-box/src/api.js
@@ -4,6 +4,7 @@ import axios from "axios"; // Importando a biblioteca axios.
 
 const api = axios.create({ // Criando uma instância do axios
 
+    //baseURL: "https://66db74c5f47a05d55be80a9c.mockapi.io/music-box" 
     baseURL: process.env.REACT_APP_API_URL 
     // process.env + NOME da variável que criamos no .env
     
